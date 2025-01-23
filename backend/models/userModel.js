d29fab3dcb0eb9371aca917634c20e0de63bd028
ankitkrjha32 <ankitkrjha32@gmail.com>
2025-01-23 21:53:14 +0530

    Admin portal changes

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -5,6 +5,7 @@ const userSchema = new mongoose.Schema({
     email: {type: String, required: true, unique: true},
     password: {type: String, required: true},
     cartData: {type: Object, default: {}},
+    profilePic: {type: String, default: "/profile.png"},
 }, {minimize: false});
 
 const userModel = mongoose.models.user || mongoose.model('user',userSchema);
